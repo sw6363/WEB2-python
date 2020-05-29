a7600cdcaaaffecbe60270132db0624dade12491
sw6363 <61794408+sw6363@users.noreply.github.com>
2020-05-29 23:42:55 +0900

    webn

---
+++ boolean.py
@@ -0,0 +1,27 @@
+#!C:\Users\LJH\AppData\Local\Programs\Python\Python38-32\python.exe
+print("content-type:text/html; charset=UTF-8\n")
+
+#number
+print(1) #Integer
+
+#string
+print('Hello World')
+
+#boolean
+print(True) #참
+print(False) #거짓
+
+#expression
+print(1+1)
+print('Hello '+'World') # = Hello World
+
+#comparison operator
+print(1==1) #True
+print(1<2) #True
+print(2<1) #False
+
+#membership operator
+print('World' in 'Hello World') #True
+
+import os.path
+print(os.path.exists('boolean.py')) #True
